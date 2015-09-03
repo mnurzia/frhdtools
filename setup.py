7d7d3ad2939a7593cb6813dab1fa217b4325cc79
maxmillion18 <max.nurzia@gmail.com>
2015-09-03 10:36:46 -0400

    pip stuff
    Whew. Finally got that over with

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+setup(name='frhdtools',
+    version='1.0.0',
+    description='Free Rider HD Tools',
+    long_description='Work with, create, and manipulate Free Rider HD Tracks.',
+    url='https://github.com/maxmillion18/frhdtools',
+    author='maxmillion18',
+    author_email='icantpostmyemailhere@none.com',
+    license='GNU GPL v3',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2.7.8'
+    ],
+    keywords='freeriderhd development',
+    packages=find_packages(),
+    package_data={},
+    data_files=[])
+    
